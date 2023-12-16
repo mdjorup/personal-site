17e75f78cd90b41f2937cc9a5b1782b5037772fd
mdjorup <michael.djorup@gmail.com>
2023-12-16 18:33:25 -0500

    revalidate changes every 5 minutes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import BlogLinks from "./BlogLinks";
 import GithubProjects from "./GithubProjects";
 import PersonalInfo from "./PersonalInfo";
 
+export const revalidate = 60 * 5;
+
 export default function Home() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-[1fr_3fr] gap-4">
